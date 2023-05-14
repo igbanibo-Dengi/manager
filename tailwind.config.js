9ccc91c8f4a2da5339869c227d74925cadc3c537
dengi <mcmorgan6560@gmail.com>
2023-05-14 14:27:06 +0100

    fixed backround images, fixed footer spacing and changed screen breakpoint values

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@
 module.exports = {
   content: ["./dist/*.{html,js}","./node_modules/flowbite/**/*.js"],
   theme: {
+    screens:{
+      sm:'480px',
+      md: '768px',
+      lg: '968px',
+      xl: '1200px'
+    },
     extend: {
       colors: {
         brightRed: 'hsl(12, 88%, 59%)',
